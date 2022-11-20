5454eb65da4ab9d5a790ddd7d87cb00d3fcb4864
ray capizzo <rcapizzo@gmail.com>
2022-11-20 16:16:28 -0500

    second commit

---
+++ src/components/Clock.js
@@ -0,0 +1,22 @@
+import { useEffect, useRef, useState } from "react";
+
+export default function Clock() {
+  const interval = useRef();
+  const [printedTime, setDate] = useState("WHO KNOWS");
+
+  useEffect(() => {
+    console.log("render");
+
+    interval.current = setInterval(() => {
+      console.log("tick");
+      var now = new Date().toLocaleString();
+      setDate(now);
+    }, 1000);
+    return () => {
+      console.log("teardown");
+      clearInterval(interval.current);
+    };
+  }, []);
+
+  return <div>{printedTime}</div>;
+}
